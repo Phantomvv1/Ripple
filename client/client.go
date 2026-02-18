8d8ca08ab23e374292e6588cfb82607b7ecee26c
Phantomvv1 <krisi_andreev@abv.bg>
2026-02-18 16:43:38 +0200

    Sterted implementing the client API and adding some features to the messages

---
+++ client/client.go
@@ -0,0 +1,65 @@
+package client
+
+import (
+	"errors"
+	"net"
+
+	"github.com/Phantomvv1/Ripple/frame"
+)
+
+type ClientConn struct {
+	net.Conn
+	authEnabled bool
+	authToken   [16]byte
+}
+
+func (c ClientConn) AuthEnabled() bool {
+	return c.authEnabled
+}
+
+func (c *ClientConn) handshake() error {
+	msg, err := frame.Decode(c)
+	if err != nil {
+		return err
+	}
+
+	if !msg.Equals(*frame.MessageWelcome) {
+		return errors.New("Error: the server has rejected the connection")
+	}
+
+	if msg.IsFlagSet(frame.AuthEnabledFlag) {
+		c.authEnabled = true
+		c.authToken = msg.AuthToken()
+	}
+
+	return nil
+}
+
+func (c *ClientConn) SendMessage(msg *frame.Message) error {
+	return frame.Encode(c, msg)
+}
+
+func (c *ClientConn) ReceiveMessage(msg *frame.Message) (*frame.Message, error) {
+	return frame.Decode(c)
+}
+
+func NewClientConn(conn net.Conn) *ClientConn {
+	return &ClientConn{Conn: conn, authEnabled: false}
+}
+
+// This function dials the server on the given port. The format of the port parameter is as follows: ":8080"
+func Dial(port string) (*ClientConn, error) {
+	conn, err := net.Dial("tcp", port)
+	if err != nil {
+		return nil, err
+	}
+
+	clientConn := NewClientConn(conn)
+
+	err = clientConn.handshake()
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
